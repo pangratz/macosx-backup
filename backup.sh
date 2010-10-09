bcb276b5af80b21b397eb3c888908d394a35e5b7
pangratz666 <pangratz666@gmail.com>
2010-10-09 14:11:03 +0200

    added many more applications and resorted the script

--- backup.sh
+++ backup.sh
@@ -15,11 +15,11 @@ function copy
 	echo "copying $1"
 	fileName="$BASE_DIR/$1"
 	if [ -d "$fileName" ]; then
-		cp -r -v "$BASE_DIR/$1/" "$TARGET_DIR/$1"
+		cp -r "$BASE_DIR/$1/" "$TARGET_DIR/$1"
 	fi
 	
 	if [ -f "$fileName" ]; then
-		cp -v "$BASE_DIR/$1" "$TARGET_DIR/$1"
+		cp "$BASE_DIR/$1" "$TARGET_DIR/$1"
 	fi	
 }
 
@@ -28,7 +28,7 @@ function copyMatchingFiles
 	echo "copying files in dir $1 starting with $2"
 	find "$BASE_DIR/$1" -maxdepth 1 -name "$2" | while read FILENAME;
 	do
-		cp -v "$FILENAME" "$TARGET_DIR/$1/";
+		cp "$FILENAME" "$TARGET_DIR/$1/";
 	done
 }
 
@@ -37,32 +37,6 @@ createDir $TARGET_DIR
 createDir $TARGET_DIR/Library
 createDir $TARGET_DIR/Music
 
-########################################
-# iTunes
-# copy Library iTunes dir
-createDir $TARGET_DIR/Library/iTunes
-copy Library/iTunes
-
-createDir $TARGET_DIR/Preferences
-copyMatchingFiles Library/Preferences "com.apple.iTunes*"
-
-# copy music directory
-createDir $TARGET_DIR/Music
-createDir $TARGET_DIR/Music/iTunes
-#copy Music/iTunes
-########################################
-
-
-########################################
-# iCal
-createDir $TARGET_DIR/Library
-createDir $TARGET_DIR/Library/Calendars
-copy Library/Calendars
-
-createDir $TARGET_DIR/Library/Preferences
-copy Library/Preferences/com.apple.iCal.helper.plist
-copy Library/Preferences/com.apple.iCal.plist
-########################################
 
 ########################################
 # Address Book
@@ -72,22 +46,6 @@ copyMatchingFiles Library/Preferences "com.apple.AddressBook*"
 
 createDir "$TARGET_DIR/Library/Application Support"
 copy "Library/Application Support/AddressBook"
-########################################
-
-########################################
-# Safari
-########################################
-
-########################################
-# Google Chrome
-########################################
-
-########################################
-# Firefox
-########################################
-
-########################################
-# Mail
 ########################################
 
 ########################################
@@ -101,15 +59,124 @@ copy Library/Widgets
 ########################################
 
 ########################################
+# Echofon
+copy Library/Preferences/com.echofon.Echofon.plist
+copy "Library/Application Support/Echofon"
+########################################
+
+########################################
+# Firefox
+createDir "$TARGET_DIR/Library/Application Support"
+copy "Library/Application Support/Firefox"
+copy Library/Preferences/org.mozilla.firefox.plist
+########################################
+
+########################################
+# Google Chrome
+copy Library/Preferences/com.google.Chrome.plist
+createDir "$TARGET_DIR/Library/Application Support/Google"
+createDir "$TARGET_DIR/Library/Application Support/Google/Chrome"
+copy "Library/Application Support/Google/Chrome"
+########################################
+
+########################################
+# iCal
+createDir $TARGET_DIR/Library
+createDir $TARGET_DIR/Library/Calendars
+copy Library/Calendars
+
+createDir $TARGET_DIR/Library/Preferences
+copy Library/Preferences/com.apple.iCal.helper.plist
+copy Library/Preferences/com.apple.iCal.plist
+########################################
+
+########################################
+# iTunes
+# copy Library iTunes dir
+createDir $TARGET_DIR/Library/iTunes
+copy Library/iTunes
+
+createDir $TARGET_DIR/Library/Preferences
+copyMatchingFiles Library/Preferences "com.apple.iTunes*"
+
+# copy music directory
+createDir $TARGET_DIR/Music
+createDir $TARGET_DIR/Music/iTunes
+#copy Music/iTunes
+########################################
+
+########################################
+# iPhoto
+createDir $TARGET_DIR/Pictures
+#copy "Pictures/iPhoto Library"
+copyMatchingFiles Library/Preferences "com.apple.iPhoto*"
+########################################
+
+########################################
+# iWork
+copy "Library/Application Support/iWork"
+copyMatchingFiles Library/Preferences "com.apple.iWork*"
+########################################
+
+########################################
 # Keychain
+copy Library/Keychain
+copy Library/Preferences/com.apple.keychainaccess.plist
 ########################################
 
 ########################################
-# Dropbox
+# Mail
+copyMatchingFiles Library/Preferences "com.apple.mail*"
+#copy Library/Mail
 ########################################
 
 ########################################
-# Notes
+# QuietRead
+copy Library/Preferences/com.bambooapps.Quiet-Read.plist
+copy "Library/Application Support/Quiet Read"
+########################################
+
+########################################
+# Safari
+copy  Library/Safari
+copyMatchingFiles Library/Safari "com.apple.Safari*"
+########################################
+
+########################################
+# ShakesPeer
+copy Library/Preferences/se.hedenfalk.shakespeer.plist
+copy "Library/Application Support/ShakesPeer"
+########################################
+
+########################################
+# Skype
+copy Library/Preferences/com.skype.skype.plist
+copy "Library/Application Support/Skype"
+########################################
+
+########################################
+# Stickies
+copy Library/StickiesDatabase
+copy Library/Preferences/com.apple.Stickies.plist
+########################################
+
+########################################
+# TextMate
+copy Library/Preferences/com.macromates.textmate.plist
+########################################
+
+########################################
+# Things
+createDir "$TARGET_DIR/Library/Application Support/Cultured Code"
+createDir "$TARGET_DIR/Library/Application Support/Cultured Code/Things"
+copy "Library/Application Support/Cultured Code/Things"
+copy Library/Preferences/com.culturedcode.things.plist
+########################################
+
+########################################
+# Transmission
+copyMatchingFiles Library/Preferences "org.m0k.transmission*"
+copy "Library/Application Support/Transmission"
 ########################################
 
 ########################################
@@ -118,6 +185,12 @@ createDir $TARGET_DIR/Library
 createDir "$TARGET_DIR/Library/Application Support"
 copy "Library/Application Support/Vienna"
 
-createDir $TARGET_DIR/Preferences
+createDir $TARGET_DIR/Library/Preferences
 copy Library/Preferences/uk.co.opencommunity.vienna2.plist
+########################################
+
+########################################
+# VLC
+copy "Library/Application Support/org.videolan.vlc"
+copyMatchingFiles Library/Preferences "org.videolan.vlc*"
 ########################################
